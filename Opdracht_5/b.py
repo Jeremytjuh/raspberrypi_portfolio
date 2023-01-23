c0927c4dc6946bcbee05056330a00768dbcf87dd
Jeremy Nelemans <jeremynelemans@gmail.com>
2023-01-23 16:15:40 +0100

    Added comments for Opdracht 5

--- Opdracht_5/b.py
+++ Opdracht_5/b.py
@@ -13,19 +13,25 @@ GPIO.setup(LED_PINS[1], GPIO.OUT)
 GPIO.setup(BTN_PINS[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BTN_PINS[1], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Geeft de tijd dat de applicatie draait terug in milliseconden
 def millis():
     return time.time() * 1000
 
 while True:
     currentTime = millis()
     for i in range(2):
+        # Als er niet op de knop geklikt wordt -> LED uit
         if GPIO.input(BTN_PINS[i]) == GPIO.HIGH:
             GPIO.output(LED_PINS[i], GPIO.LOW)
+        # Als er wel op de knop geklikt wordt
         else:
+            # Als er 1000 milliseconden gepasseerd is sinds de laatste keer dat de LED geknipperd heeft
             if currentTime - lastTimes[i] >= knipperTimes[i]:
                 lastTimes[i] = currentTime
+                # Als de led al aan stond -> zet hem uit
                 if ledStatus[i] == GPIO.HIGH:
                     ledStatus[i] = GPIO.LOW
+                # Als de led uit stond -> zet hem aan
                 else:
                     ledStatus[i] = GPIO.HIGH
                 GPIO.output(LED_PINS[i], ledStatus[i])
--- Opdracht_5/c.py
+++ Opdracht_5/c.py
@@ -13,34 +13,44 @@ GPIO.setup(LED_PINS[0], GPIO.OUT)
 GPIO.setup(LED_PINS[1], GPIO.OUT)
 GPIO.setup(BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Geeft de tijd dat de applicatie draait terug in milliseconden
 def millis():
     return time.time() * 1000
 
 while True:
     currentTime = millis()
+    # Als er niet op de knop geklikt wordt
     if GPIO.input(BTN_PIN) == GPIO.HIGH:
+        # Als er 1000 milliseconden gepasseerd is sinds de laatste keer dat de LED geknipperd heeft
         if currentTime - lastTime >= knipperTime:
             lastTime = currentTime
+            # Als led 1 al aan stond -> zet hem uit en zet led 2 aan
             if ledStatus[0] == GPIO.HIGH:
                 ledStatus[0] = GPIO.LOW
                 ledStatus[1] = GPIO.HIGH
+            # Als led 1 uit stond -> zet hem aan en zet led 2 uit
             else:
                 ledStatus[0] = GPIO.HIGH
                 ledStatus[1] = GPIO.LOW
             for i in range(2):
                 GPIO.output(LED_PINS[i], ledStatus[i])
     else:
+        # Als er een set aantal milliseconden gepasseerd is sinds de laatste keer dat de LED geknipperd heeft
         if currentTime - lastTime >= knipperTimePressed:
             lastTime = currentTime
+            # Als led 1 al aan stond -> zet hem uit en zet led 2 aan
             if ledStatus[0] == GPIO.HIGH:
                 ledStatus[0] = GPIO.LOW
                 ledStatus[1] = GPIO.HIGH
 
+                # Verander de knippertijd naar 700 milliseconden
                 knipperTimePressed = 700
+            # Als led 1 uit stond -> zet hem aan en zet led 2 uit
             else:
                 ledStatus[0] = GPIO.HIGH
                 ledStatus[1] = GPIO.LOW
 
+                # Verander de knippertijd naar 1300 milliseconden
                 knipperTimePressed = 1300
             for i in range(2):
                 GPIO.output(LED_PINS[i], ledStatus[i])
