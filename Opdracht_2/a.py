bd2aa33c64e1d8c756c67e79636de2d089a0e227
Jeremy Nelemans <jeremynelemans@gmail.com>
2023-01-23 15:57:19 +0100

    Added comments for Opdracht 2

--- Opdracht_2/a.py
+++ Opdracht_2/a.py
@@ -1,13 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIO Pin waarop de LED is aangesloten
 LED_PIN = 16
 
+# Configureren van de Raspberry Pi voor het gebruik van de GPIO pins
 GPIO.setmode(GPIO.BCM)
+# Instellen van de pin als uitvoer in plaats van invoer
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 while True:
+    # Aanzetten van de LED
     GPIO.output(LED_PIN, GPIO.HIGH)
+    # Delay van 1 seconden
     time.sleep(1)
+    # Uitzetten van de LED
     GPIO.output(LED_PIN, GPIO.LOW)
+    # Delay van 1 seconden
     time.sleep(1)
--- Opdracht_2/d.py
+++ Opdracht_2/d.py
@@ -1,13 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIO Pin waarop de LED is aangesloten
 LED_PIN = 16
 
+# Configureren van de Raspberry Pi voor het gebruik van de GPIO pins
 GPIO.setmode(GPIO.BCM)
+# Instellen van de pin als uitvoer in plaats van invoer
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 while True:
+    # Aanzetten van de LED
     GPIO.output(LED_PIN, GPIO.HIGH)
+    # Delay van 0.001 seconden
     time.sleep(0.001)
+    # Uitzetten van de LED
     GPIO.output(LED_PIN, GPIO.LOW)
+    # Delay van 0.001 seconden
     time.sleep(0.001)
